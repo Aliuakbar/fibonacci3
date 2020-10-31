eea481b58ca066a4cea3a01ef8e97c06afbf49ed
Aliukabar <aligottschall22@gmail.com>
2020-10-31 21:01:13 +0100

    Read the display size dynamically

--- fibonacci3/main.py
+++ fibonacci3/main.py
@@ -3,53 +3,54 @@ from i3ipc.aio import Connection
 import asyncio
 
 PHI = (1 + 5**(1 / 2)) / 2
-WIDTH = 1920
-HEIGHT = 1080
 
-async def on_focus_window(i3, e):
-    tree = await i3.get_tree()
+class Fibonacci3:
 
-    con = tree.find_focused()
-    root =  con.root()
-
-    rect = con.rect
-    width = rect.width
-    height = rect.height
-
-    # new_width = int(min(width * PHI, 1920 / PHI))
-    new_width = int(WIDTH / PHI)
-    # new_height = int(min(height * PHI, 1080 / PHI))
-    new_height = int(HEIGHT / PHI)
-
-    step = 20
-    # Annoying animation :)
-    """
-    while new_width > width:
-        width += step
-        result = await con.command(f"resize grow width {step}")
-    while new_width < width:
-        width -= step
-        result = await con.command(f"resize shrink width {step}")
-    while new_height > height:
-        height += step
-        result = await con.command(f"resize grow height {step}")
-    while new_height < height:
-        height -= step
-        result = await con.command(f"resize shrink height {step}")
-    """
-    result = await con.command(f"resize set width {new_width}")
-    result = await con.command(f"resize set height {new_height}")
+    async def run(self):
+        i3 = await Connection().connect()
+        tree = await i3.get_tree()
+        rect = tree.workspaces()[0].rect
+        self.width = rect.width
+        self.height = rect.height
+        i3.on(Event.WINDOW_FOCUS, self.on_focus_window)
+        i3.on(Event.WINDOW_NEW, self.on_focus_window)
+        i3.on(Event.WORKSPACE_FOCUS, self.on_workspace_focus)
+        await i3.main()
 
 
-async def fibonacci3():
-    i3 = await Connection().connect()
-    i3.on(Event.WINDOW_FOCUS, on_focus_window)
-    i3.on(Event.WINDOW_NEW, on_focus_window)
-    await i3.main()
+    async def on_focus_window(self, i3, e):
+        tree = await i3.get_tree()
+        con = tree.find_focused()
+        new_width = int(self.width / PHI)
+        new_height = int(self.height / PHI)
+
+        step = 20
+        # Annoying animation :)
+        """
+        while new_width > width:
+            width += step
+            result = await con.command(f"resize grow width {step}")
+        while new_width < width:
+            width -= step
+            result = await con.command(f"resize shrink width {step}")
+        while new_height > height:
+            height += step
+            result = await con.command(f"resize grow height {step}")
+        while new_height < height:
+            height -= step
+            result = await con.command(f"resize shrink height {step}")
+        """
+        result = await con.command(f"resize set width {new_width}")
+        result = await con.command(f"resize set height {new_height}")
+
+    def on_workspace_focus(self, i3, e):
+        self.width = e.current.rect.width
+        self.heigth = e.current.rect.height
 
 def main():
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(fibonacci3())
+    f3 = Fibonacci3()
+    loop.run_until_complete(f3.run())
 
 if __name__ == "__main__":
     main()
